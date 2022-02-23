00aefb3db95d995485eaa42ad21cad8e5ac81b79
Marek Suchánek <marek.suchanek@gmail.com>
2022-02-23 17:00:01 +0100

    Init DSW data seeder

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    long_description = ''.join(f.readlines())
+
+setup(
+    name='dsw_data_seeder',
+    version='0.1.0',
+    description='Worker for seeding DSW data',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Marek Suchánek',
+    keywords='data seed database storage',
+    license='Apache License 2.0',
+    url='https://github.com/ds-wizard/mailer',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Database',
+        'Topic :: Text Processing',
+        'Topic :: Utilities',
+    ],
+    zip_safe=False,
+    python_requires='>=3.9, <4',
+    install_requires=[
+        'click',
+        'minio',
+        'psycopg2',
+        'PyYAML',
+        'tenacity',
+    ],
+    entry_points={
+        'console_scripts': [
+            'dsw-seeder=dsw_data_seeder:main',
+        ],
+    },
+)
